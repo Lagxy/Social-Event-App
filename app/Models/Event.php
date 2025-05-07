0ac754ae84552ce5fcb9895341f91220d6103f3f
Lagxy <lexysamuel1507@gmail.com>
2025-05-07 11:12:30 +0700

    add event related page

---
+++ app/Models/Event.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class Event {
+    public static function all(){
+
+    }
+}
+
+?>
---
+++ resources/views/components/event.blade.php
@@ -0,0 +1,13 @@
+<div class="event-card">
+    <div class="event-thumbnail">
+        <img src="" alt="event thumbnail">
+    </div>
+    <div class="event-title">
+        <h3>Event name</h3>
+        <p>by [username]</p>
+    </div>
+    <div class="event-interaction">
+        <button>Join</button>
+        <button>Detail</button>
+    </div>
+</div>
